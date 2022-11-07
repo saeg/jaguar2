4afaa722849a892c913428ed06b08efba26af595
Roberto Araujo <roberto.andrioli@gmail.com>
2022-11-07 18:38:05 -0300

    Spectrum export service provider interface

---
+++ jaguar2-api/src/main/java/br/usp/each/saeg/jaguar2/SpectrumExporterLoader.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2021, 2021 University of Sao Paulo and Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roberto Araujo - initial API and implementation and/or initial documentation
+ */
+package br.usp.each.saeg.jaguar2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+import br.usp.each.saeg.jaguar2.spi.SpectrumExporter;
+
+/**
+ * A spectrum exporter service loading facility.
+ */
+public class SpectrumExporterLoader {
+
+    private final ServiceLoader<SpectrumExporter> serviceLoader;
+
+    public SpectrumExporterLoader(
+            final ServiceLoader<SpectrumExporter> serviceLoader) {
+        this.serviceLoader = serviceLoader;
+    }
+
+    public SpectrumExporterLoader() {
+        this(ServiceLoader.load(SpectrumExporter.class));
+    }
+
+    /**
+     * Loads and returns all spectrum exporter services available.
+     *
+     * This implementation returns a {@link Collection} of
+     * {@link SpectrumExporter} available from underlying
+     * {@link ServiceLoader}.
+     *
+     * @return a collection of spectrum export services.
+     */
+    public Collection<SpectrumExporter> load() {
+        final Collection<SpectrumExporter> exporters = new ArrayList<SpectrumExporter>();
+        final Iterator<SpectrumExporter> iterator = serviceLoader.iterator();
+        while (iterator.hasNext()) {
+            exporters.add(iterator.next());
+        }
+        return exporters;
+    }
+
+}
---
+++ jaguar2-api/src/test/java/br/usp/each/saeg/jaguar2/DummySpectrumExporter.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2021, 2021 University of Sao Paulo and Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roberto Araujo - initial API and implementation and/or initial documentation
+ */
+package br.usp.each.saeg.jaguar2;
+
+import br.usp.each.saeg.jaguar2.api.IClassSpectrum;
+import br.usp.each.saeg.jaguar2.api.SpectrumEval;
+import br.usp.each.saeg.jaguar2.spi.SpectrumExporter;
+
+public class DummySpectrumExporter implements SpectrumExporter {
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void write(final IClassSpectrum spectrum, final SpectrumEval eval) {
+    }
+
+    @Override
+    public void shutdown() {
+    }
+
+}
