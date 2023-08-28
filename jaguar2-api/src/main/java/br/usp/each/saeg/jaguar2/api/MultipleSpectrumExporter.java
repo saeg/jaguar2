b5549f7ffa81acc79c1e32850600b75454a15f90
Roberto Araujo <roberto.andrioli@gmail.com>
2023-08-28 19:22:18 -0300

    Create `MultipleSpectrumExporter` class

---
+++ jaguar2-api/src/main/java/br/usp/each/saeg/jaguar2/api/MultipleSpectrumExporter.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2021, 2021 University of Sao Paulo and Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roberto Araujo - initial API and implementation and/or initial documentation
+ */
+package br.usp.each.saeg.jaguar2.api;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import br.usp.each.saeg.jaguar2.spi.SpectrumExporter;
+
+public class MultipleSpectrumExporter implements SpectrumExporter {
+
+    private final Collection<SpectrumExporter> exporters;
+
+    public MultipleSpectrumExporter(final Collection<SpectrumExporter> exporters) {
+        this.exporters = exporters;
+    }
+
+    @Override
+    public void init() throws Exception {
+        for (final SpectrumExporter exporter : exporters) {
+            exporter.init();
+        }
+    }
+
+    @Override
+    public void write(final IClassSpectrum spectrum, final SpectrumEval eval)
+            throws Exception {
+
+        for (final SpectrumExporter exporter : exporters) {
+            exporter.write(spectrum, eval);
+        }
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+        for (final SpectrumExporter exporter : exporters) {
+            exporter.shutdown();
+        }
+    }
+
+    public Collection<SpectrumExporter> getExporters() {
+        return Collections.unmodifiableCollection(exporters);
+    }
+
+}
